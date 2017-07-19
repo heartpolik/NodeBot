aa299f6c15a12a0c7ad18d24fdc99f446640e32c
Constantine Polischuk <heart.polik@gmail.com>
2017-07-19 21:56:09 +0300

    fixes

--- index.js
+++ index.js
@@ -4,20 +4,27 @@ let token = '433991323:AAF7fBkNsOdVBajNeMymTfEsb53pAuFW4i0';
 
 let bot = new plusBot(token, {polling : true});
 
+let js_answers =
+    [
+        'Don`t tell me about JS, you, son of a bitch!',
+        'let a = "foo"'
+    ];
+
 //repaet after me
-bot.onText('\/start', function (msg) {
+bot.onText('/\/start/', function (msg) {
     bot.sendMessage(msg.from.id, 'Hello!');
 });
 
 bot.onText( /[ ,.]js\b/i , function (msg, match) {
     var fromId = msg.from.id;
-    var resp = match[1];
+    var index = math.floor(math.random(js_answers.length));
+    var resp = js_answers[index];
     bot.sendMessage(fromId, resp);
 });
 
-bot.onText(/(.+)js(.+)/, function (msg) {
-    let fromId = msg.from.id;
-    let resp = 'Don`t tell me about JS, you, son of a bitch!';
-    bot.sendMessage(fromId, resp);
-});
+// bot.onText(/(.+)js(.+)/, function (msg) {
+//     let fromId = msg.from.id;
+//     let resp = '';
+//     bot.sendMessage(fromId, resp);
+// });
 
